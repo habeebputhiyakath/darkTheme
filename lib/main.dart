134614359f6559ad41d7c8179cdd0ca84090a2e8
habeebputhiyakath <irfanputhiyakat@gmail.com>
2023-09-25 11:13:03 +0530

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:light_dark_mode/controller/theme_provider.dart';
 import 'package:light_dark_mode/theme/theme.dart';
 import 'package:light_dark_mode/view/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) {
     WidgetsFlutterBinding.ensureInitialized();
-  runApp(myapp());
+  runApp(ChangeNotifierProvider(create:(context) =>  ThemeProvider(),
+  child: myapp(),
+  ));
 }
 class myapp extends StatelessWidget {
   const myapp({super.key});
@@ -13,9 +17,10 @@ class myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Homepage(),
-     theme: lightMode,
+    //  theme: lightMode,
        
-      darkTheme: darkMode,
+     // darkTheme: darkMode,
+    theme: Provider.of<ThemeProvider>(context).themeData,
      
      
     );
